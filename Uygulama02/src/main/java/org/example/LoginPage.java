db4a55b9dacfd7b9f93abb2c48cf2b6a904f6b31
YavuzSelimCanıbek <142090293+YavuzSelimCnbk@users.noreply.github.com>
2025-02-14 18:03:47 +0300

    Kullanıcı adı alanı oluşturuldu

--- Uygulama02/src/main/java/org/example/LoginPage.java
+++ Uygulama02/src/main/java/org/example/LoginPage.java
@@ -6,13 +6,32 @@ import java.awt.*;
 public class LoginPage {
 
     JFrame frame;
+    JPanel userPanel;
+    JLabel userLabel;
+    JTextField userNameField;
 
     public LoginPage()
     {
+
+        userPanel = new JPanel();
+        userPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        userLabel = new JLabel();
+        userLabel.setText("Kullanıcı İsmi");
+        userPanel.add(userLabel);
+
+        userNameField = new JTextField();
+        userNameField.setPreferredSize(new Dimension(300, 25));
+        userPanel.add(userNameField);
+
+
+
         frame = new JFrame();
         frame.setSize(500,500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new GridLayout(3,1));
+
+        frame.add(userPanel);
+
         frame.setVisible(true);
 
 
